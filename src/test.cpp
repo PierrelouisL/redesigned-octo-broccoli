a86cbb0b1e75f694f3aad9c5bd724dbf7b36ed52
Pierre-Louis <pierrelouis.lagunegrand@gmail.com>
2021-12-16 18:19:03 +0100

    t

--- src/test.cpp
+++ src/test.cpp
@@ -1,33 +1,104 @@
 #include <SFML/Graphics.hpp>
 
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+
+    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+    {
+        // on charge la texture du tileset
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // on redimensionne le tableau de vertex pour qu'il puisse contenir tout le niveau
+        m_vertices.setPrimitiveType(sf::Quads);
+        m_vertices.resize(width * height * 4);
+
+        // on remplit le tableau de vertex, avec un quad par tuile
+        for (unsigned int i = 0; i < width; ++i)
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // on récupère le numéro de tuile courant
+                int tileNumber = tiles[i + j * width];
+
+                // on en déduit sa position dans la texture du tileset
+                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+                // on récupère un pointeur vers le quad à définir dans le tableau de vertex
+                sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+                // on définit ses quatre coins
+                quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+                quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+                // on définit ses quatre coordonnées de texture
+                quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+                quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            }
+
+        return true;
+    }
+
+private:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // on applique la transformation
+        states.transform *= getTransform();
+
+        // on applique la texture du tileset
+        states.texture = &m_tileset;
+
+        // et on dessine enfin le tableau de vertex
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // on crée la fenêtre
+    sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
 
+    // on définit le niveau à l'aide de numéro de tuiles
+    const int level[] =
+    {
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
+
+    // on crée la tilemap avec le niveau précédemment défini
+    TileMap map;
+    if (!map.load("tileset.png", sf::Vector2u(32, 32), level, 16, 8))
+        return -1;
+
+    // on fait tourner la boucle principale
     while (window.isOpen())
     {
+        // on gère les évènements
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if(event.type == sf::Event::Closed)
                 window.close();
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            // left key is pressed: move our character
-            shape.move(10.f, 0.f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            // left key is pressed: move our character
-            shape.move(-10.f, 0.f);
-        }
-        
 
+        // on dessine le niveau
         window.clear();
-        window.draw(shape);
+        window.draw(map);
         window.display();
     }
 
