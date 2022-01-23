8134e1e01211fb5d4177cd5aae320a549aa4ea9d
Pierre-Louis <pierrelouis.lagunegrand@gmail.com>
2022-01-23 19:47:25 +0100

    avec bot'

--- src/Map/main_map.cpp
+++ src/Map/main_map.cpp
@@ -102,191 +102,192 @@ int main(){
     bool heal = false;
     element.sound_LoadStart(music, "sound/AnimalCrossing.wav", 80.f, true);
     while (window->isOpen() && !quit){
-        WinMutex.lock();
-        bots.check_and_follow(ptr_perso->getPosition());
-        while (window->pollEvent(event)){
-            if(event.type == sf::Event::Closed || (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Escape)){
-                printf("C'est chao\n");
-                window->close();
-            }
+        if(g_mode == normal || g_mode == mario){
+            WinMutex.lock();
+            while (window->pollEvent(event)){
+                if(event.type == sf::Event::Closed || (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Escape)){
+                    printf("C'est chao\n");
+                    window->close();
+                    quit = true;
+                }
 
-            if(event.type == sf::Event::KeyPressed){
-                if(event.key.code == sf::Keyboard::A){  // Action key = A
+                if(event.type == sf::Event::KeyPressed){
+                    if(event.key.code == sf::Keyboard::A){  // Action key = A
 
-                    sf::Vector2f next_case = ptr_perso->checkFrontCase(4, false);                  // Tree spawn
-                    if( next_case != sf::Vector2f(-1, -1) ){
+                        sf::Vector2f next_case = ptr_perso->checkFrontCase(4, false);                  // Tree spawn
+                        if( next_case != sf::Vector2f(-1, -1) ){
 
-                        //ptr_perso->heal(); // Spawning a tree makes you get healed
+                            //ptr_perso->heal(); // Spawning a tree makes you get healed
 
-                        element.sound_LoadStart(sound_effect, "sound/Tree.wav", 25.f, false);
+                            element.sound_LoadStart(sound_effect, "sound/Tree.wav", 25.f, false);
 
-                        obstacle_ville1[ abs((int) ((next_case.y)/64)) ][ abs((int) (next_case.x/64)) ] = -1;
+                            obstacle_ville1[ abs((int) ((next_case.y)/64)) ][ abs((int) (next_case.x/64)) ] = -1;
 
-                        TileMap temp_element;
-                        int level[] = { 0, 1 };
-                        
-                        temp_element.load("images/Decors/arbre.png", level, 1, 2);
-                        temp_element.setPosition(sf::Vector2f((int) (next_case.x/64)*64, (int) ((next_case.y)/64)*64) + sf::Vector2f(-2, -80));         
-                        element.put_VectorElement(temp_element);
-                        element.put_VectorType(4);
+                            TileMap temp_element;
+                            int level[] = { 0, 1 };
+                            
+                            temp_element.load("images/Decors/arbre.png", level, 1, 2);
+                            temp_element.setPosition(sf::Vector2f((int) (next_case.x/64)*64, (int) ((next_case.y)/64)*64) + sf::Vector2f(-2, -80));         
+                            element.put_VectorElement(temp_element);
+                            element.put_VectorType(4);
 
-                        allGoal.add_tree();
-                        if(allGoal.get_goalTree()){
-                            map.change_map("images/Map/Map3.png");
-                            map.load_map();
+                            allGoal.add_tree();
+                            if(allGoal.get_goalTree()){
+                                map.change_map("images/Map/Map3.png");
+                                map.load_map();
 
-                            for(int i=0; i<7; i++){
-                                obstacle_ville1[10][19+i] = -2;
+                                for(int i=0; i<7; i++){
+                                    obstacle_ville1[10][19+i] = -2;
+                                }
                             }
                         }
+                        switch(g_mode){
+                            case normal:
+                                if(bots.current_bot()->alive){
+                                    g_mode = fight;
+                                }
+
+                                if( ptr_perso->checkFrontCase(-2, false) != sf::Vector2f(-1, -1) ){             // City to Trump's room 
+                                    element.sound_LoadStart(music, "sound/HymneEtatUnis.wav", 20.f, true);
+                                    view.setCenter(sf::Vector2f(7*64, 57.5*64));
+                                
+                                }
+                                else if( ptr_perso->checkFrontCase(-3, false) != sf::Vector2f(-1, -1) ){        // Trump's room to City
+                                    element.sound_LoadStart(music, "sound/AnimalCrossing.wav", 80.f, true);
+                                    view.setCenter(sf::Vector2f(22*64, 11.5*64));
+                                
+                                }           
+                                else if( ptr_perso->checkFrontCase(3, false) != sf::Vector2f(-1, -1) ){         // Camera event
+                                    if(ptr_perso->get_eye() == TileCharacter::Left){
+                                        allGoal.done_goalCamera();
+                                        element.sound_LoadStart(sound_effect, "sound/discours_Greta.wav", 80.f, false);
+                                        while(music.getStatus() == sf::Music::Playing);
+                                    }
+                                }
+                                else if( ptr_perso->checkFrontCase(10, false) != sf::Vector2f(-1, -1) ){        // TP at the start of mario event
+                                    g_mode = mario;
+                                    view.setCenter(sf::Vector2f(22*64, 56.5*64));
+                                    element.sound_LoadStart(music, "sound/SuperMarioBros.wav", 50.f, true);
+                                    ptr_perso = &persoMario;
+                                }
+                                break;
+
+                            case mario :
+
+                                if( ptr_perso->checkFrontCase(11, false) != sf::Vector2f(-1, -1) ){         // TP to House
+                                    element.sound_LoadStart(music, "sound/AnimalCrossing.wav", 80.f, true);
+
+                                    g_mode = normal;
+                                    view.setCenter(sf::Vector2f(4.5*64, 30.5*64));
+                                    music.stop();
+                                    ptr_perso = &perso;
+                                }
+
+                                else if( ptr_perso->checkFrontCase(12, false) != sf::Vector2f(-1, -1) ){
+                                    clk.restart();            
+                                    element.sound_LoadStart(sound_effect, "sound/MushroomSound.wav", 5.f, false);
+                                    for(int i=0; i<4; i++){
+                                        ptr_perso->scale( sf::Vector2f(2, 2));
+                                        ptr_perso->setPosition( view.getCenter()+sf::Vector2f(-64*2*(i*i+1), -64*2*(i+1+1)) );
+                                        window->setActive();
+                                        window->clear();
+                                        window->draw(map);
+                                        window->draw(*ptr_perso);
+                                        window->display();
+                                        clk.restart();
+                                        while(clk.getElapsedTime().asSeconds() < 2);
+                                    }
+                                    element.sound_LoadStart(sound_effect, "sound/BallonBoom.wav", 80.f, false);                                
+                                    ptr_perso->setScale( sf::Vector2f(1, 1));
+                                    view.setCenter(sf::Vector2f(22*64, 56.5*64));
+                                }  
+                                break;
+
+                            case fight :
+                                if(!bots.current_bot()->alive){
+                                    g_mode = normal;
+                                }
+                                break;
+                            case menu_:
+                                // We don't do anything here
+                                break;
+                            default :
+                                break;
+                        }
                     }
-                    switch(g_mode){
-                        case normal:
-                            if(bots.current_bot()->alive){
-                                g_mode = fight;
-                            }
 
-                            if( ptr_perso->checkFrontCase(-2, false) != sf::Vector2f(-1, -1) ){             // City to Trump's room 
-                                element.sound_LoadStart(music, "sound/HymneEtatUnis.wav", 20.f, true);
-                                view.setCenter(sf::Vector2f(7*64, 57.5*64));
-                            
-                            }
-                            else if( ptr_perso->checkFrontCase(-3, false) != sf::Vector2f(-1, -1) ){        // Trump's room to City
-                                element.sound_LoadStart(music, "sound/AnimalCrossing.wav", 80.f, true);
-                                view.setCenter(sf::Vector2f(22*64, 11.5*64));
-                            
-                            }           
-                            else if( ptr_perso->checkFrontCase(3, false) != sf::Vector2f(-1, -1) ){         // Camera event
-                                if(ptr_perso->get_eye() == TileCharacter::Left){
-                                    allGoal.done_goalCamera();
-                                    element.sound_LoadStart(sound_effect, "sound/discours_Greta.wav", 80.f, false);
-                                    while(music.getStatus() == sf::Music::Playing);
+                    
+                    if(ptr_perso->oneMoveFlag()){
+                        sf::Vector2f next_case;
+                        switch(g_mode){
+
+                            case normal :
+                                next_case = ptr_perso->checkFrontCase(5, false);                  // Car event
+                                if( next_case != sf::Vector2f(-1, -1) ){
+
+                                    element.sound_LoadStart(sound_effect, "sound/Car.wav", 80.f, false);  
+
+                                    obstacle_ville1[ abs((int) ((next_case.y)/64)) ][ abs((int) (next_case.x/64)) ] = 0;
+
+                                    TileMap temp_element;
+                                    int level[] = { 0, 1, 2, 3};
+                                    
+                                    temp_element.load("images/Decors/voiture.png", level, 2, 2);
+                                    temp_element.setPosition(sf::Vector2f((int) (next_case.x/64)*64, (int) ((next_case.y)/64)*64) + sf::Vector2f(640, -64));        
+                                    element.put_VectorElement(temp_element);
+                                    element.put_VectorType(5);
                                 }
-                            }
-                            else if( ptr_perso->checkFrontCase(10, false) != sf::Vector2f(-1, -1) ){        // TP at the start of mario event
-                                g_mode = mario;
-                                view.setCenter(sf::Vector2f(22*64, 56.5*64));
-                                element.sound_LoadStart(music, "sound/SuperMarioBros.wav", 50.f, true);
-                                ptr_perso = &persoMario;
-                            }
-                            break;
+                                next_case = ptr_perso->checkFrontCase(6, false);
+                                if(  next_case != sf::Vector2f(-1, -1) ){
 
-                        case mario :
-
-                            if( ptr_perso->checkFrontCase(11, false) != sf::Vector2f(-1, -1) ){         // TP to House
-                                element.sound_LoadStart(music, "sound/AnimalCrossing.wav", 80.f, true);
-
-                                g_mode = normal;
-                                view.setCenter(sf::Vector2f(4.5*64, 30.5*64));
-                                music.stop();
-                                ptr_perso = &perso;
-                            }
-
-                            else if( ptr_perso->checkFrontCase(12, false) != sf::Vector2f(-1, -1) ){
-                                clk.restart();            
-                                element.sound_LoadStart(sound_effect, "sound/MushroomSound.wav", 5.f, false);
-                                for(int i=0; i<4; i++){
-                                    ptr_perso->scale( sf::Vector2f(2, 2));
-                                    ptr_perso->setPosition( view.getCenter()+sf::Vector2f(-64*2*(i*i+1), -64*2*(i+1+1)) );
-                                    window->setActive();
-                                    window->clear();
-                                    window->draw(map);
-                                    window->draw(*ptr_perso);
-                                    window->display();
-                                    clk.restart();
-                                    while(clk.getElapsedTime().asSeconds() < 2);
+                                    obstacle_ville1[ abs((int) ((next_case.y)/64)) ][ abs((int) (next_case.x/64)) ] = 0;
+                                    
+                                    element.sound_LoadStart(sound_effect, "sound/SpecialSoundEffect.wav", 80.f, false);  
+                                    view.zoom(3);
                                 }
-                                element.sound_LoadStart(sound_effect, "sound/BallonBoom.wav", 80.f, false);                                
-                                ptr_perso->setScale( sf::Vector2f(1, 1));
-                                view.setCenter(sf::Vector2f(22*64, 56.5*64));
-                            }  
-                            break;
+                                next_case = ptr_perso->checkFrontCase(8, false);
+                                if( next_case != sf::Vector2f(-1, -1)){
+                                    g_mode = fight;
+                                }
+                                break;
 
-                        case fight :
-                            if(!bots.current_bot()->alive){
-                                g_mode = normal;
-                            }
-                            break;
-                        case menu_:
-                            // We don't do anything here
-                            break;
-                        default :
-                            break;
+                            case mario : 
+                                break;
+                            default :
+                                break;
+                        }
                     }
                 }
-
-                
-                if(ptr_perso->oneMoveFlag()){
-                    sf::Vector2f next_case;
-                    switch(g_mode){
-
-                        case normal :
-                            next_case = ptr_perso->checkFrontCase(5, false);                  // Car event
-                            if( next_case != sf::Vector2f(-1, -1) ){
-
-                                element.sound_LoadStart(sound_effect, "sound/Car.wav", 80.f, false);  
-
-                                obstacle_ville1[ abs((int) ((next_case.y)/64)) ][ abs((int) (next_case.x/64)) ] = 0;
-
-                                TileMap temp_element;
-                                int level[] = { 0, 1, 2, 3};
-                                
-                                temp_element.load("images/Decors/voiture.png", level, 2, 2);
-                                temp_element.setPosition(sf::Vector2f((int) (next_case.x/64)*64, (int) ((next_case.y)/64)*64) + sf::Vector2f(640, -64));        
-                                element.put_VectorElement(temp_element);
-                                element.put_VectorType(5);
-                            }
-                            next_case = ptr_perso->checkFrontCase(6, false);
-                            if(  next_case != sf::Vector2f(-1, -1) ){
-
-                                obstacle_ville1[ abs((int) ((next_case.y)/64)) ][ abs((int) (next_case.x/64)) ] = 0;
-                                
-                                element.sound_LoadStart(sound_effect, "sound/SpecialSoundEffect.wav", 80.f, false);  
-                                view.zoom(3);
-                            }
-                            break;
-
-                        case mario : 
-                            break;
-
-                        case fight :
-                            break;
-
-                        default :
-                            break;
-                    }
-                }
-            }
-    
-            ptr_perso->checkKeyMove(event);  // Check status of movement key
-            //bots.check_and_follow(perso);
-        }
-        window->setActive();
-        ptr_perso->move(view);           // Move character
-
-        // on dessine le niveau
-        window->setView(view);
-        window->clear();
-        window->draw(map);
-        bots.draw(*window);
-
-        ptr_perso->setPosition( view.getCenter()+sf::Vector2f(-64, -64) );   // Set the middle of the character in the middle of the view
-        //std::cout << "x=" << (int)view.getCenter().x/64 << "y = " << (int)view.getCenter().y/64 << std::endl;
-        //bots.check_and_follow(perso);
-        //perso2.setPosition(sf::Vector2f(192,2304 ));
-        //window.draw(perso2);
-        window->draw(*ptr_perso);
-
-        element.load_allElement(*window);
         
-        window->draw(map_decors);
-        allGoal.display_goal(*window, view.getCenter());
+                ptr_perso->checkKeyMove(event);  // Check status of movement key
+                //bots.check_and_follow(perso);
+            }
+            window->setActive();
+            ptr_perso->move(view);           // Move character
 
-        window->display();
-        WinMutex.unlock();
+            // on dessine le niveau
+            window->setView(view);
+            window->clear();
+            window->draw(map);
+            bots.draw(*window);
+
+            ptr_perso->setPosition( view.getCenter()+sf::Vector2f(-64, -64) );   // Set the middle of the character in the middle of the view
+            //std::cout << "x=" << (int)view.getCenter().x/64 << "y = " << (int)view.getCenter().y/64 << std::endl;
+            //bots.check_and_follow(perso);
+            //perso2.setPosition(sf::Vector2f(192,2304 ));
+            //window.draw(perso2);
+            window->draw(*ptr_perso);
+
+            element.load_allElement(*window);
+            
+            window->draw(map_decors);
+            allGoal.display_goal(*window, view.getCenter());
+
+            window->display();
+            WinMutex.unlock();
+        }
     }
     std::cout << "le vrai chao!"<< std::endl;
-    exit(0);
     //thread.wait();
     delete window;
     return 0;
